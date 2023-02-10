5bc7b0799f9fc412c0072ae014f07c7da8b56537
Nikita <ALEX83625527@yandex.ru>
2023-02-10 16:17:07 +0300

    ïgnore

---
+++ number1/Program.cs
@@ -0,0 +1,75 @@
+﻿//Console.WriteLine("Введите Числа A ");
+//int Value = int.Parse(Console.ReadLine());
+//int sum = Value*Value;
+//Console.WriteLine("Квадрат числа A равен: " + sum);
+
+//Console.WriteLine("Введите Число A ");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите Число B ");
+//int b = int.Parse(Console.ReadLine());
+//int max = 0;
+//int min = 0;
+//if (a>b) {
+//max = a; min = b;
+//}
+//else {
+//max = b; min = a;
+//}
+//Console.WriteLine("Ответ Макс: " + max + " Ответ Мин: " + min);
+
+
+//Console.WriteLine("Введите Число A ");
+//int a = int.Parse(Console.ReadLine());
+//int b = -a;
+//Console.Write(b + ",");
+//while (a > b)
+//{
+//   b = b + 1;
+//   Console.Write(b + ",");
+//}
+
+//Console.WriteLine("Введите Число A ");
+//int a = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите Число B ");
+//int b = int.Parse(Console.ReadLine());
+//Console.WriteLine("Введите Число С ");
+//int c = int.Parse(Console.ReadLine());
+//int max = 0;
+//if (a>b) {
+//max = a; 
+//}
+//else {
+//max = b;
+//}
+//if (c>max) {
+//max = c;
+//}
+//Console.WriteLine("Ответ Макс: " + max );
+
+//Console.WriteLine("Введите Число A ");
+//int a = int.Parse(Console.ReadLine());
+//if (a % 2 == 1){
+    //Console.WriteLine("Нечетное число") ;
+//}
+//else {
+//Console.WriteLine("Четное число" );
+//}
+
+//Console.WriteLine("Введите Число A ");
+//int a = int.Parse(Console.ReadLine());
+//int b = 1;
+//while (b<a) { 
+    //if ( b % 2 == 1) {
+    //}
+    //else {
+   //     Console.WriteLine("Ответ : " + b );
+    //}
+    //b = b + 1;
+    
+//}
+
+
+
+
+
+
