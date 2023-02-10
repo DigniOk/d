b31b7023096ba7f1a7d1e59f2e96365f7f92523d
Nikita <ALEX83625527@yandex.ru>
2023-02-10 17:05:04 +0300

    Home4

---
+++ Home3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите Число A ");
+int a = int.Parse(Console.ReadLine());
+int b = 1;
+while (b<a) { 
+    if ( b % 2 == 1) {
+    }
+    else {
+       Console.WriteLine("Ответ : " + b );
+    }
+    b = b + 1;
+}
