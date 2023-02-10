7623ed99b9424f689343a05eb04f0202d83c5e38
Nikita <ALEX83625527@yandex.ru>
2023-02-10 16:50:14 +0300

    Home1

---
+++ Home/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Введите Число A ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Число B ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите Число С ");
+int c = int.Parse(Console.ReadLine());
+int max = 0;
+if (a>b) {
+max = a; 
+}
+else {
+max = b;
+}
+if (c>max) {
+max = c;
+}
+Console.WriteLine("Ответ Макс: " + max );
