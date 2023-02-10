1aafcf609e31518fa1e29e4cd4c72796a92960e9
Nikita <ALEX83625527@yandex.ru>
2023-02-10 17:00:39 +0300

    Home3

---
+++ Home2/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите Число A ");
+int a = int.Parse(Console.ReadLine());
+if (a % 2 == 1){
+    Console.WriteLine("Нечетное число") ;
+}
+else {
+Console.WriteLine("Четное число" );
+}
