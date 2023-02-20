737e6f70127667284a293c420eefe4111012e348
Nikita <ALEX83625527@yandex.ru>
2023-02-20 14:40:44 +0300

    Home3.3

---
+++ Home3.3/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите X: ");
+int x = int.Parse(Console.ReadLine());
+int i = 1;
+int n = x;
+if (x < 0) {
+i = x;
+n = -x;
+x = -1;
+}
+int[] result = new int[n];
+int j = 0;
+for(; i<=x;i++){
+result[j] = i * i * i;
+    if (result[j]<0){
+        result[j] = result[j] * (-1);
+    }
+j++;
+}
+for(j=0; j < n; j++) {
+Console.WriteLine(result[j]);
+}
