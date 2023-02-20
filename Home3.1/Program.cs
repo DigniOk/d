05af75d5f7eec0d5c91785c4c1abae90c9d780e9
Nikita <ALEX83625527@yandex.ru>
2023-02-20 14:05:39 +0300

    Hone3.1

---
+++ Home3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите X: ");
+string x = Console.ReadLine();
+if (x.Length<5 || x.Length>5)  {
+Console.WriteLine("Число не пятизначное");
+return;
+}
+if (x[0] == x[4] && x[1] == x[3]) {
+    Console.WriteLine("Число палидром");
+}
+else {
+    Console.WriteLine("Число не палидром");
+}
