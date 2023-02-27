81ecf16dcdd77f39015fc689ddc089e784878bae
Nikita <ALEX83625527@yandex.ru>
2023-02-27 16:58:53 +0300

    Home4.2

---
+++ Home4.2/Program.cs
@@ -0,0 +1,13 @@
+﻿//Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("Введите A: ");
+int a = int.Parse(Console.ReadLine());
+int SumG(int a)
+{
+    int sum = 0;
+    while (a > 0) {
+        sum = sum + (a%10);
+        a = a / 10;
+    }
+    return sum;
+}
+Console.Write($"Сумма цифр чсила {a} равна {SumG(a)}");
