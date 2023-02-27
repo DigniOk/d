e7f81ea5804b086ce9eb275854c8949be40b5a2b
Nikita <ALEX83625527@yandex.ru>
2023-02-27 17:00:45 +0300

    Home4.3

---
+++ Home4.3/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+int[] GetBinaryArray(int size){
+    int[] result = new int[size];
+    for (int i = 0; i < size; i++){
+        result[i] = new Random().Next(10, 51);
+    }
+    return result;
+}
+int[] array = GetBinaryArray(8);
+Console.WriteLine($"[  {String.Join(",   ", array)}  ]");
