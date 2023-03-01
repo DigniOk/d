8cc4c81840f2b1716373bcdb19cc2364762acebc
Nikita <ALEX83625527@yandex.ru>
2023-03-01 15:35:23 +0300

    Home5.2

---
+++ Home5.2/Program.cs
@@ -0,0 +1,30 @@
+﻿//Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] array(int size)
+{
+    int[] arr = new int[size];
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(-10, 10);
+    }
+    return arr;
+}
+int[] reserved(int[] array)
+{
+
+    int A = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i % 2 == 1)
+        {
+            A = A + array[i];
+        }
+    }
+    int[] sums = { A };
+    return sums;
+}
+Console.WriteLine("Введите разрядность массива ");
+int size = int.Parse(Console.ReadLine());
+int[] mass = array(size);
+Console.WriteLine($"[{String.Join(", ", mass)}]");
+Console.WriteLine($" Сумма нечетных индексов {String.Join(", ", reserved(mass))}");
