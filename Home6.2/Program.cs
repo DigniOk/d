92db6f72d37b563f6aa043769f9d711b01b3ee80
Nikita <ALEX83625527@yandex.ru>
2023-03-03 17:01:58 +0300

    Home6.2

---
+++ Home6.2/Program.cs
@@ -0,0 +1,16 @@
+﻿//Написать программу Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+Console.WriteLine("Введите числа");
+int[] values = Array.ConvertAll < string, int >( Console.ReadLine().Split(new char[] {' ', ','}, StringSplitOptions.RemoveEmptyEntries), x=> int.Parse(x) );
+Console.WriteLine($"Положительных чисел: {String.Join(" ", ggg(values))} ");
+int[] ggg(int[] inArray)
+{
+    int b = 0;
+    for (int i = 0; i < inArray.Length; i++) 
+    {
+        if (inArray[i] > 0) {
+        b = b + 1;
+        }
+    }
+   int[] sums = { b };
+   return sums;
+}
