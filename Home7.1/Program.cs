ebdfb75b48f8eda024813d3a1a5d59dbe20230ad
Nikita <ALEX83625527@yandex.ru>
2023-03-08 15:13:36 +0300

    Home7.1

---
+++ Home7.1/Program.cs
@@ -0,0 +1,32 @@
+﻿// //Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Ввелите количество строк массива");
+int rows = int.Parse(Console.ReadLine());
+Console.WriteLine("Ввелите количество столбцов массива");
+int columns = int.Parse(Console.ReadLine());
+double[,] array = GetArray(rows, columns);
+PrintArray(array);
+double[,] GetArray(int m, int n)
+{
+        double[,] result = new double[m, n];
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                result[i, j] =  new Random().NextDouble() * 100;
+            }
+
+        }
+        return result;
+}
+void PrintArray(double[,] inArray)
+{
+    for(int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for(int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($" {inArray[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
