15c5c6dadf06af20624a6da296dd42c3b04001c1
Nikita <ALEX83625527@yandex.ru>
2023-03-26 17:46:22 +0300

    projectmd3

--- project/Program.cs
+++ project/Program.cs
@@ -18,7 +18,7 @@ void PrintArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {   
-        Console.Write($"{array[i]}");
+        Console.Write($"{array[i]} ");
     }
     Console.WriteLine();
 }
