84e5347ed91dab6107bff61249fd50309fcd5371
Nikita <ALEX83625527@yandex.ru>
2023-03-26 16:38:14 +0300

    project

---
+++ project/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите число или слово через запятую: ");
+string[] TextArray = Console.ReadLine().Split(',');
+string[] NewTextArray = new string[TextArray.Length];
+void ArraySearch(string[] TextArray, string[] NewTextArray)
+{
+    int count = 0;
+    for (int i = 0; i < TextArray.Length; i++)
+    {
+    if(TextArray[i].Length <= 3)
+        {
+        NewTextArray[count] = TextArray[i];
+        count = count + 1;
+        }
+    }
+}
