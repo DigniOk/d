b05ced73619c718251b5635712bdac40679e975f
Nikita <ALEX83625527@yandex.ru>
2023-06-20 11:18:19 +0300

    dz25py

---
+++ JAVA/Home3.java
@@ -0,0 +1,23 @@
+package JAVA;
+import java.util.ArrayList;
+import java.util.Random;
+public class Home3 {
+  
+    public static void main(String[] args) {
+        
+        ArrayList<Integer> list = new ArrayList<>();
+        for (int i = 0; i < 5;i++) {
+            list.add(new Random().nextInt(15));
+        }
+        System.out.println(list);
+        int min = list.get(0);
+        for (int i = list.size() - 1; i >= 0; i--) {
+                if (list.get(i) < min) {
+                    min = list.get(i);
+             }
+        }
+
+      System.out.println("Минимальное значениe = "+min);  
+    }
+}
+
